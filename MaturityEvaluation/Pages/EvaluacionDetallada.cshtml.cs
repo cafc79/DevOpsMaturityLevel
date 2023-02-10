8ba5c99ec2e870628fc88c49cadd1e40643a522d
César Flores <cafc79@yahoo.com>
2023-02-09 23:34:03 -0600

    Add files via upload

---
+++ MaturityEvaluation/Pages/EvaluacionDetallada.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MaturityEvaluation.Pages
+{
+    public class EvaluacionDetalladaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
