68e2e7b7ffff6ebac84b037c9fac955b68aadf25
César Flores <cafc79@yahoo.com>
2023-02-16 16:20:59 -0600

    Add files via upload

--- MaturityEvaluation/Pages/EvalResult.cshtml.cs
+++ MaturityEvaluation/Pages/EvalResult.cshtml.cs
@@ -1,6 +1,7 @@
 using MaturityEvaluation.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace MaturityEvaluation.Pages
 {
@@ -10,13 +11,27 @@ namespace MaturityEvaluation.Pages
         public string mTitle { get; set; }
 
         [BindProperty]
+        public string mLevel { get; set; }
+
+        [BindProperty]
         public string mDescripcion { get; set; }
 
         public void OnGet()
         {            
             EvaluationT1 ev = new EvaluationT1();
             mTitle = Request.Query["stage"];
-            mDescripcion = ev.GetResult(Request.Query["stage"]);            
+            mDescripcion = ev.GetResult(Request.Query["stage"]);
+            switch (mTitle) {
+                case "Ad-Hoc":
+                    mLevel = "alert-danger";
+                    break;
+                case "Defined":
+                    mLevel = "alert-warning";
+                    break;
+                default:
+                    mLevel = "alert-success";
+                    break;
+            }
         }
     }
 }
